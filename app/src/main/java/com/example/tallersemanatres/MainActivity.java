75b15ffbe4276efaf0c1b57849f7d4f43bced7f5
Deivy Rodriguez <deivy.rodriguez@correo.policia.gov.co>
2022-11-12 20:06:42 -0500

    Imagen y Boton

--- app/src/main/java/com/example/tallersemanatres/MainActivity.java
+++ app/src/main/java/com/example/tallersemanatres/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.tallersemanatres;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    MaterialButton btnIngresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIngresar = findViewById(R.id.btnIngresar);
+
+        //
+        btnIngresar.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
