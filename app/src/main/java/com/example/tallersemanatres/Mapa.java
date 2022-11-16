b6ef320701d5fb6a196541bb3d8c0b8e24c5e1a6
Deivy Rodriguez <deivy.rodriguez@correo.policia.gov.co>
2022-11-16 10:04:49 -0500

    v 0.0.7 mapa 40%

--- app/src/main/java/com/example/tallersemanatres/Mapa.java
+++ app/src/main/java/com/example/tallersemanatres/Mapa.java
@@ -2,13 +2,29 @@ package com.example.tallersemanatres;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.views.MapView;
 
 public class Mapa extends AppCompatActivity {
+
+    MapView mapa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
+
+        mapa = findViewById(R.id.mapa);
+
+        //Obtener el contexto
+        Context ctx = getApplicationContext();
+
+        //Configuracion
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
     }
 }
